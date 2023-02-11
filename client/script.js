0c7b34c8a83c50ad50f463b91b959c002fbe40cd
Joeyy0415 <125135294+Joeyy0415@users.noreply.github.com>
2023-02-11 21:36:01 +0700

    Update script.js

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ async function handleSubmit(e) {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://brainbot.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
